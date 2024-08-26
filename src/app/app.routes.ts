922fd2c2a75dc6a8d72ccf205012d0a912805509
walisonbraga <walison.lemos@gmail.com>
2024-08-25 21:58:58 -0500

    termina perte de Habilidades e Experiência em Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  }
+];
