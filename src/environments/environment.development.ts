34018993ccbbf0697121894327655eae8aa1f859
walisonbraga <walison.lemos@gmail.com>
2024-08-28 21:46:36 -0500

    portfolio terminado

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyA4lS8t22siW4wmCyNibO7_y7ozZnAiviw",
+    authDomain: "walison-development.firebaseapp.com",
+    projectId: "walison-development",
+    storageBucket: "walison-development.appspot.com",
+    messagingSenderId: "428548241665",
+    appId: "1:428548241665:web:3c9303ec01688a9bc20300",
+    measurementId: "G-K8Z0M6GE00"
+  },
+  production: true,
+};
